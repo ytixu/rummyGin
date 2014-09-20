c16f2332ae1e9b7332d2ad162465bec0db6d677d
Wayne Lee <wayneleeyk@users.noreply.github.com>
2014-09-20 15:16:18 -0400

    Added more tests for Card class

--- Rummy-M1/test/ca/mcgill/cs/comp303/rummy/model/TestCard.java
+++ Rummy-M1/test/ca/mcgill/cs/comp303/rummy/model/TestCard.java
@@ -3,6 +3,7 @@ package ca.mcgill.cs.comp303.rummy.model;
 import static org.junit.Assert.*;
 import ca.mcgill.cs.comp303.rummy.model.Card.Rank;
 import ca.mcgill.cs.comp303.rummy.model.Card.Suit;
+import ca.mcgill.cs.comp303.rummy.testutils.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,5 +29,22 @@ public class TestCard
 	public void testHashCode()
 	{
 		assertEquals(39, aCard.hashCode());
+	}
+	
+	@Test
+	public void testCompareTo()
+	{
+		assertEquals(1, aCard.compareTo(AllCards.CAC));
+		assertEquals(0, aCard.compareTo(AllCards.CAS));
+		assertEquals(-1, aCard.compareTo(AllCards.CKS));
+	}
+	
+	@Test
+	public void testEquals()
+	{
+		assertEquals(true, aCard.equals(AllCards.CAS));
+		assertEquals(false, aCard.equals(null));
+		// Random object used to compared
+		assertEquals(false, aCard.equals(AllCards.CAS.getClass()));
 	}
 }
