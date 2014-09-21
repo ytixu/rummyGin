9977a8958e16a4c5cfd1f597d9338b0fc11ee9e4
ytixu <yi.t.xu@mail.mcgill.ca>
2014-09-21 00:58:10 -0400

    So we both modified this...

---
+++ Rummy-M1/test/ca/mcgill/cs/comp303/rummy/model/TestCard.java
@@ -0,0 +1,60 @@
+package ca.mcgill.cs.comp303.rummy.model;
+
+import static org.junit.Assert.*;
+import ca.mcgill.cs.comp303.rummy.model.Card.Rank;
+import ca.mcgill.cs.comp303.rummy.model.Card.Suit;
+import ca.mcgill.cs.comp303.rummy.testutils.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCard
+{
+	private Card aCard;
+	
+	@Before
+	public void setup() 
+	{
+		aCard = new Card(Rank.ACE, Suit.SPADES);
+	}
+
+	@Test
+	public void testConstructor()
+	{
+		assertEquals(Rank.ACE, aCard.getRank());
+		assertEquals(Suit.SPADES, aCard.getSuit());
+	}
+	
+	@Test
+	public void testHashCode()
+	{
+		assertEquals(39, aCard.hashCode());
+	}
+	
+	@Test
+	public void testCompareAndEqual()
+	{
+		Card samllCard = new Card(Rank.ACE, Suit.CLUBS);
+		assertEquals(0, aCard.compareTo(aCard));
+		assertEquals(1, aCard.compareTo(samllCard));
+		assertEquals(true, aCard.equals(aCard));
+		assertEquals(false, aCard.equals(samllCard));
+	}
+	
+	@Test
+	public void testCompareTo()
+	{
+		assertEquals(1, aCard.compareTo(AllCards.CAC));
+		assertEquals(0, aCard.compareTo(AllCards.CAS));
+		assertEquals(-1, aCard.compareTo(AllCards.CKS));
+	}
+	
+	@Test
+	public void testEquals()
+	{
+		assertEquals(true, aCard.equals(AllCards.CAS));
+		assertEquals(false, aCard.equals(null));
+		// Random object used to compared
+		assertEquals(false, aCard.equals(AllCards.CAS.getClass()));
+	}
+}
