2f5f11992e4ca78f5ffae0205de3d7b947e300da
yitian <yi.t.xu@mail.mcgill.ca>
2014-11-06 17:36:49 -0500

    More code

--- Rummy-M1/src/ca/mcgill/cs/comp303/rummy/gui/javafx/Gui.java
+++ Rummy-M1/src/ca/mcgill/cs/comp303/rummy/gui/javafx/Gui.java
@@ -4,9 +4,12 @@ import java.io.File;
 
 import ca.mcgill.cs.comp303.rummy.model.gameModel.GameEngine;
 import ca.mcgill.cs.comp303.rummy.model.gamePlayers.Player;
+import ca.mcgill.cs.comp303.rummy.model.gamePlayers.RandomPlayer;
+import ca.mcgill.cs.comp303.rummy.model.gamePlayers.RobotPlayer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -30,16 +33,22 @@ public class Gui extends Application
 	private static final int SIZE_X = 1600;
 	private static final int SIZE_Y = 800;
 	private static final int BOX_SPACE = 5;
+	private static final int PADDING = 10;
+	private static final String DEFAULTNAME = "G";
+	
+	private static final String CHOOSENAME = "Please choose a name.";
+	private static final String CHOOSELEVEL = "Please choose a level.";
 	
 	private final GameEngine CHICKEN = new GameEngine();
 	private final StackPane ROOT = new StackPane();
 	
 	private Player aHumanPlayer;
+	private OnlineObserver aLogDisplay;
 	
-	private enum Level{
+	private enum Level
+	{
 		WOBBUFFET, PSYDUCK
 	}
-	private Level aLevel;
 
 	/*
 	 * setup initial positions of the cards
@@ -52,7 +61,7 @@ public class Gui extends Application
 	/**
 	 * get name
 	 */
-	private Pane initialize()
+	private Pane getPlayer()
 	{
 		HBox levelPane = new HBox(BOX_SPACE);
 		levelPane.setAlignment(Pos.CENTER);
@@ -64,14 +73,13 @@ public class Gui extends Application
 			ToggleButton lev = new ToggleButton(l.name());
 			if (l.ordinal() == 0)
 			{
-				aLevel = l;
 				lev.setSelected(true);
 			}
 			levelPane.getChildren().add(lev);
 			lev.setToggleGroup(levGroup);
 		}
 		
-		TextField nameInput = new TextField("Choose a name.");
+		TextField nameInput = new TextField(CHOOSENAME);
 		nameInput.setMaxSize(200, 20);
 		
 		Button submit = new Button("Enter");
@@ -82,17 +90,33 @@ public class Gui extends Application
 				ToggleButton bnt = (ToggleButton) levGroup.getSelectedToggle();
 				if (bnt == null)
 				{
-					error.setText("Please choose a level.");
+					error.setText(CHOOSELEVEL);
 					return;
 				}
+				Level level = null;
 				for (Level l : Level.values())
 				{
 					if (l.name().equals(bnt.getText()))
 					{
-						aLevel = l;
+						level = l;
 					}
 				}
-				aHumanPlayer = new FXHumanPlayer(nameInput.getText());
+				assert level != null;
+				String name = nameInput.getText();
+				System.out.println(name.equals(CHOOSENAME));
+				if (name.equals("") || name.equals(CHOOSENAME))
+				{
+					name = DEFAULTNAME;
+				}
+				aHumanPlayer = new FXHumanPlayer(name);
+				if (level.equals(Level.WOBBUFFET))
+				{
+					CHICKEN.setPlayers(aHumanPlayer, new RandomPlayer());
+				}
+				if (level.equals(level.PSYDUCK))
+				{
+					CHICKEN.setPlayers(aHumanPlayer, new RobotPlayer());
+				}
 				drawEverything();
 			}
 		});
@@ -101,8 +125,8 @@ public class Gui extends Application
 		pane.setAlignment(Pos.CENTER);
 		pane.getChildren().add(levelPane);
 		pane.getChildren().add(nameInput);
-		pane.getChildren().add(submit);
 		pane.getChildren().add(error);
+		pane.getChildren().add(submit);
 		return pane;
 	}
 	
@@ -125,21 +149,32 @@ public class Gui extends Application
 		return menu;
 	}
 	
+	/*
+	 * Draw everything that are not related to the cards. 
+	 */
 	private void drawEverything()
-	{
-		ROOT.setAlignment(Pos.TOP_LEFT);
-		Pane manu = getMenu();
+	{	
+		HBox pane = new HBox(BOX_SPACE);
+		VBox leftBar = new VBox(BOX_SPACE);
+		leftBar.setAlignment(Pos.BOTTOM_LEFT);
+		aLogDisplay.displayDisplay(leftBar);
+		leftBar.getChildren().add(getMenu());
         
+		pane.getChildren().add(leftBar);
 		ROOT.getChildren().clear();
-		ROOT.getChildren().add(manu);
+		ROOT.getChildren().add(pane);
 	}
 	
 	@Override
     public void start(Stage pStage) 
-	{
-		pStage.setTitle("G");
+	{	
+		// initialization
+		pStage.setTitle("Gin Rummy");
+		aLogDisplay = new OnlineObserver();
+		CHICKEN.addObservers(aLogDisplay);
+		ROOT.setPadding(new Insets(PADDING));
 		// get name of player
-		ROOT.getChildren().add(initialize());
+		ROOT.getChildren().add(getPlayer());
 		
 		// css
 //        File f = new File("style.css");
---
+++ Rummy-M1/src/ca/mcgill/cs/comp303/rummy/gui/javafx/OnlineObserver.java
@@ -0,0 +1,101 @@
+package ca.mcgill.cs.comp303.rummy.gui.javafx;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import ca.mcgill.cs.comp303.rummy.model.ICardSet;
+import ca.mcgill.cs.comp303.rummy.model.gameModel.GameModelLogger;
+import ca.mcgill.cs.comp303.rummy.model.gameModel.GameObserver;
+
+/**
+ * Observer that write on the GUI.
+ * @author ytixu
+ *
+ */
+public class OnlineObserver implements GameObserver 
+{
+	private final int aX = 300;
+	private final int aY = 200;
+	private TextField aDisplayer = new TextField();
+	private Label[] aPlayerScores;
+	
+	/**
+	 * Add the text displayer to the GUI.
+	 * @param pPane
+	 */
+	public void displayDisplay(Pane pPane)
+	{
+		aDisplayer.setMinSize(aX, aY);
+		aDisplayer.setDisable(true);
+		aDisplayer.setAlignment(Pos.BOTTOM_LEFT);
+		for (Label l : aPlayerScores)
+		{
+			pPane.getChildren().add(l);
+		}
+		pPane.getChildren().add(aDisplayer);
+	}
+	
+	private void write(String text)
+	{
+		aDisplayer.setText(aDisplayer.getText() + text + "\n");
+	}
+	
+	@Override
+	public void logPlayed(GameModelLogger pEngine) 
+	{
+		write(pEngine.getTrun() + " played");
+		Object knock = pEngine.getKnocked();
+		Object layout = pEngine.getLayout();
+		if (layout != null)
+		{
+			write("Layout.");
+			for (ICardSet s : (Set<ICardSet>) layout)
+			{
+				write(s.toString());
+			}
+		}
+		else if (knock != null)
+		{
+			write("Player knocked.");
+			for (ICardSet s : (Set<ICardSet>) knock)
+			{
+				write(s.toString());
+			}
+		}
+		else
+		{
+			write("The top of the dicard pile is " + pEngine.getDiscard().toString());
+		}
+	}
+
+	@Override
+	
+	public void logEndGame(GameModelLogger pEngine) 
+	{
+		write("Score:\n" + pEngine.getScore().toString());
+		write("Game Ended");
+	}
+
+	/**
+	 * @pre pEngine.getPlayers() returns an array of length 2
+	 */
+	@Override
+	public void logStartGame(GameModelLogger pEngine) 
+	{
+		HashMap<String, Integer> score = (HashMap<String, Integer>)pEngine.getScore();
+		aPlayerScores = new Label[score.size()];
+		int i = 0;
+		for (Entry<String, Integer> s : score.entrySet())
+		{
+			aPlayerScores[i] = new Label(s.getKey() + " : " + s.getValue());
+			i++;
+		}
+		String[] names = pEngine.getPlayers();
+		write(names[0] + " vs " + names[1]); 
+	}
+}
